49c0e506e9a2ed005b3f1fc8fadb2f8b55b63096
Lavergne <hugo.lavergne@devinci.fr>
2019-09-29 00:04:11 +0200

    set files for gallery

--- routes/main.js
+++ routes/main.js
@@ -1,20 +1,21 @@
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID});
 const router = vertex.router();
+const ProjectController = require('../controllers/ProjectController')
 
 
 router.get('/', (req, res) => {
-    const data = {
-        image_profile: 'https://i.ytimg.com/vi/qvXZcqv1C3U/maxresdefault.jpg'  ,
-        greeting: 'Hello ! Welcome to my Portfolio site !',
-        introduction: "I'm a web development student and a freelancer working on the development of website and webapp. Expert Laravel ",
-        languages: [
-            {name: 'Laravel', years: 3},
-            {name: 'Angular', years: 2},
-            {name: 'NodeJs', years: 1},
-            {name: 'Wordpress', years: 2}
-        ]
-    }
-    res.render('landing', data);
+    const data = req.context; 
+    
+    const projectCtr = new ProjectController();
+    projectCtr.get()
+    .then(projects => {
+        data['projects'] = projects;
+        res.render('landing', data);
+    })
+    .catch(err => {
+        res.send('Oops ! ' + err.message)
+    }) 
+    
 })
 
 module.exports = router
