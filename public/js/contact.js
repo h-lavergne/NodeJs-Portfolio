c96039a771ccd04c034d6bb3c543c26f1a015c7c
Lavergne <hugo.lavergne@devinci.fr>
2019-09-30 16:48:19 +0200

    fix

--- public/js/contact.js
+++ public/js/contact.js
@@ -15,6 +15,15 @@
         console.log('Check that all the field are filled out')
         const errormessage = "Your message couldn't be sent, please check that all the fields are filled out.";
         $("#errormessage").text(errormessage);
+        if(visitor.name === "") {
+          $('#contact-form-name').addClass("border border-danger")
+        } else if(visitor.email === "") {
+          $('#contact-form-email').addClass("border border-danger")
+        } else {
+          $('#contact-form-message').addClass("border border-danger")
+        }
+        
+        
       }
         
       else{
@@ -25,9 +34,13 @@
           data: visitor,
           success: function (res) {
             $("#errormessage").text("");
-            console.log('SUBSCRIBER CREATED: ' + JSON.stringify(res))
+            console.log('SUBSCRIBER CREATED: ' + JSON.stringify(res));
+            $('#contact-form-name').removeClass("border border-danger").val("");
+            $('#contact-form-email').removeClass("border border-danger").val("");
+            $('#contact-form-message').removeClass("border border-danger").val("");
             const successmessage = 'Your message has been succesfully sent';
             $("#successmessage").text(successmessage);
+            
           },
           error: function (res) {
             res.status(400);
