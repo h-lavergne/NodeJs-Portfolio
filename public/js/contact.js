7bfb9250d86dc8c305856f8b7f470e4c1f744267
Lavergne <hugo.lavergne@devinci.fr>
2019-09-30 16:21:25 +0200

    improvment

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,30 +1,39 @@
-(function() {
-    console.log("HELLO");
-    $('#contact-submit').click(function(event) {
-      if (event) {
-        event.preventDefault()
-        
-        var visitor = {
-          name: $('#contact-form-name').val(),
-          email: $('#contact-form-email').val(),
-          message: $('#contact-form-message').val()
-        }
+(function () {
+  console.log("HELLO");
+  $('#contact-submit').click(function (event) {
+    if (event) {
+      event.preventDefault()
 
-        console.log('Contact form submitted ' + JSON.stringify(visitor));
-
-      $.ajax({
-        url: '/api/subscriber',
-        type: 'POST',
-        data: visitor,
-        success: function(res){
-          console.log('SUBSCRIBER CREATED: ' + JSON.stringify(res))
-          successmessage = 'Your message has been succesfully sent';
-          $("#successmessage").text(successmessage);
-        },
-        error:function(res){
-          
-        }
-      })
+      var visitor = {
+        name: $('#contact-form-name').val(),
+        email: $('#contact-form-email').val(),
+        message: $('#contact-form-message').val()
       }
-    })
-  })()
+
+      if (visitor.name === "" || visitor.email === "" || visitor.message === "") {
+        $("#successmessage").text("");
+        console.log('Check that all the field are filled out')
+        const errormessage = "Your message couldn't be sent, please check that all the fields are filled out.";
+        $("#errormessage").text(errormessage);
+      }
+        
+      else{
+        console.log('Contact form submitted ' + JSON.stringify(visitor));
+        $.ajax({
+          url: '/api/subscriber',
+          type: 'POST',
+          data: visitor,
+          success: function (res) {
+            $("#errormessage").text("");
+            console.log('SUBSCRIBER CREATED: ' + JSON.stringify(res))
+            const successmessage = 'Your message has been succesfully sent';
+            $("#successmessage").text(successmessage);
+          },
+          error: function (res) {
+            res.status(400);
+          }
+        })
+      }
+    }
+  })
+})()
